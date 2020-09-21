fcca96e04e2e5265cbc192c858a28f1fc868840d
miriamino <gitlab_miriamino@fantabulous.pink>
2020-09-21 22:36:16 +0200

    fixed errors and deprecation warning...whoopsie ^2

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 const config = require('./utils/config')
+require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-require('express-async-errors')
 
 const app = express()
 
